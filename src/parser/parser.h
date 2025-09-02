dd536f03243b007fc124aa7c12c997655497bc71
Blopaa <pabloosabaterr@gmail.com>
2025-09-02 19:55:44 +0200

    refactor: refactor parsing expression functions

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -8,59 +8,92 @@
 #define PARSER_H
 
 typedef enum {
-     STRING_VARIABLE_DEFINITION,
-     INT_VARIABLE_DEFINITION,
-     FLOAT_VARIABLE_DEFINITION,
-     BOOL_VARIABLE_DEFINITION,
-     ADD_OP,
-     SUB_OP,
-     MUL_OP,
-     DIV_OP,
-     MOD_OP,
-     INT_LIT,
-     FLOAT_LIT,
-     STRING_LIT,
-     BOOL_LIT,
-     VARIABLE,
-     ASSIGNMENT,
-     PRE_INCREMENT,
-     PRE_DECREMENT,
-     POST_INCREMENT,
-     POST_DECREMENT,
-     COMPOUND_ADD_ASSIGN,
-     COMPOUND_SUB_ASSIGN,
-     COMPOUND_MUL_ASSIGN,
-     COMPOUND_DIV_ASSIGN,
-     null_NODE
-} NodeTypes ;
+    STRING_VARIABLE_DEFINITION,
+    INT_VARIABLE_DEFINITION,
+    FLOAT_VARIABLE_DEFINITION,
+    BOOL_VARIABLE_DEFINITION,
+    ADD_OP,
+    SUB_OP,
+    MUL_OP,
+    DIV_OP,
+    MOD_OP,
+    INT_LIT,
+    FLOAT_LIT,
+    STRING_LIT,
+    BOOL_LIT,
+    VARIABLE,
+    ASSIGNMENT,
+    PRE_INCREMENT,
+    PRE_DECREMENT,
+    POST_INCREMENT,
+    POST_DECREMENT,
+    COMPOUND_ADD_ASSIGN,
+    COMPOUND_SUB_ASSIGN,
+    COMPOUND_MUL_ASSIGN,
+    COMPOUND_DIV_ASSIGN,
+    null_NODE
+} NodeTypes;
+
+typedef enum {
+    PREC_NONE,
+    PREC_OR, // || (for future)
+    PREC_AND, // && (for future)
+    PREC_EQUALITY, // == != (for future)
+    PREC_COMPARISON, // < > <= >= (for future)
+    PREC_TERM, // + -
+    PREC_FACTOR, // * / %
+    PREC_UNARY, // ! - ++ --
+    PREC_PRIMARY // literals, variables
+} Precedence;
 
 typedef struct {
-     TokenType TkType;
-     NodeTypes type;
+    TokenType token;
+    NodeTypes nodeType;
+    Precedence precedence;
+} OperatorInfo;
+
+static const OperatorInfo operators[] = {
+    {TokenSum, ADD_OP, PREC_TERM},
+    {TokenSub, SUB_OP, PREC_TERM},
+    {TokenMult, MUL_OP, PREC_FACTOR},
+    {TokenDiv, DIV_OP, PREC_FACTOR},
+    {TokenMod, MOD_OP, PREC_FACTOR},
+    {TokenNULL, null_NODE, PREC_NONE}
+};
+
+
+typedef struct {
+    TokenType TkType;
+    NodeTypes type;
 } NodeMap;
 
 
 static const NodeMap TypeDefs[] = {
-     {TokenStringDefinition, STRING_VARIABLE_DEFINITION},
-     {TokenIntDefinition, INT_VARIABLE_DEFINITION},
-     {TokenFloatDefinition, FLOAT_VARIABLE_DEFINITION},
-     {TokenBoolDefinition, BOOL_VARIABLE_DEFINITION},
-     {TokenNULL, null_NODE}
+    {TokenStringDefinition, STRING_VARIABLE_DEFINITION},
+    {TokenIntDefinition, INT_VARIABLE_DEFINITION},
+    {TokenFloatDefinition, FLOAT_VARIABLE_DEFINITION},
+    {TokenBoolDefinition, BOOL_VARIABLE_DEFINITION},
+    {TokenNULL, null_NODE}
 };
 
 
 struct ASTNode {
-     char * value;
-     NodeTypes NodeType;
-     struct ASTNode * children;
-     struct ASTNode * brothers;
+    char *value;
+    NodeTypes NodeType;
+    struct ASTNode *children;
+    struct ASTNode *brothers;
 };
 
-typedef struct ASTNode * ASTNode;
+typedef struct ASTNode *ASTNode;
+
 NodeTypes getDecType(TokenType type);
-ASTNode createNode(char* val, NodeTypes type);
+
+ASTNode createNode(char *val, NodeTypes type);
+
 ASTNode ASTGenerator(Token token);
+
 void printAST(ASTNode node, int depth);
+
 void freeAST(ASTNode node);
 
 #endif //PARSER_H
